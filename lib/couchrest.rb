09da29159c2c3d229bc1ca6e5cdf1fa81d6b00a3
Mark Rada <mrada@marketcircle.com>
2011-03-11 13:18:04 -0500

    Require restclient as it is the most up to date name

--- lib/couchrest.rb
+++ lib/couchrest.rb
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-require 'rest_client'
+require 'restclient'
 require 'json'
 
 # Not sure why this is required, so removed until a reason is found!
